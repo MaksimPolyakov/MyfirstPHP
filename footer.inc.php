e2e5e04b205967685a57b56f7d20d7bc1174f064
MaksimPolyakov <110829496+MaksimPolyakov@users.noreply.github.com>
2022-12-21 22:32:06 +0300

    Add files via upload

---
+++ footer.inc.php
@@ -0,0 +1,3 @@
+<div class="footer">
+            <h2>Мы изучили осоновы PHP!</h2>
+</div>
---
+++ index.php
@@ -0,0 +1,20 @@
+
+
+<?php
+ $p = 'Приветсвую Вас на моей страничке!';
+?>
+
+<?php
+ $name = 'Maksim';
+ $surname = 'Poliakov';
+ $city = 'Moscow';
+ $age = 31;
+?>
+
+
+<?php
+include 'main.php';
+?>
+
+<!--echo '<p>Приветсвую Вас на моей страничке!</p>'; -->
+ 
